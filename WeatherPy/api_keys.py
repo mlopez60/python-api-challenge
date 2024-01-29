599327792d34e75b8196ce50681efe37ed5b0f7b
mlopez60 <mlopez@MacBook-Pro.local>
2024-01-28 19:58:16 -0500

    Part 1 push

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "Test"
+weatherkey= "ec7e6740c19b6b4ce40ea2a814d25333"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "6da6dcaefe664d549c2f4959da7f4b94"
